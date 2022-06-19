3c831842cf51490c1a135d259cccdb24203b4608
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-06-19 14:52:08 +0300

    Text content changed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',init)
+
+function init() {
+    const pText = document.querySelector('p')
+    pText.textContent = 'This is really cool!'
+    return pText
+
+}
+init(pText)
+
+// text changed
